dbd5204675b0a2f0e2abf00cd30ada1dba37be49
Vishesh Nag <vishesh.nag@here.com>
2021-03-01 20:15:48 +0530

    Added changes in python.jinja2 and test case. Code flow is able to reach till __setattr__()

--- flatdata-py/tests/test_archive_creation.py
+++ flatdata-py/tests/test_archive_creation.py
@@ -5,13 +5,20 @@ from common import DictResourceStorage, INSTANCE_TEST_SCHEMA, RESOURCE_PAYLOAD, 
 def test_create_empty_archive():
     module = Engine(INSTANCE_TEST_SCHEMA).render_python_module()
     dict_storage = DictResourceStorage()
-
     builder = module.backward_compatibility_ArchiveBuilder(dict_storage)
     # is setting some dict fine for setting a struture, or
     # should we require it to be the specific class?
-    builder.set("resource", {"a": 0, "b": 0, "c": 0, "d": 0})
+    #builder.set("resource", {"a": 0, "b": 0, "c": 0, "d": 0})
 
     # do we need something like a finish/close method?
     # if we do, should we try to make it work with python's
     # `with ... as archive_builder:` syntax?
     #builder.finish()
+    class dummy:
+        def __init__(self):
+            self.a = -0x1
+            self.b=0x01234567
+            self.c=-0x28
+            self.d=0
+
+    builder.set_resource(dummy())
